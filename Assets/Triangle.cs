601e3d37ad4eba3ca7c71661c57fab61a6fff19b
Mark <umbreon27@gmail.com>
2016-04-16 04:12:02 -0400

    Initial project and triangle maker.
    Initial project. Tiny script to make triangles.

---
+++ Assets/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Triangle : MonoBehaviour {
+
+    [Range(0, 3)]
+    public int direction = 0;
+
+    public bool regenerate = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (regenerate)
+        {
+            BuildTriangle();
+            regenerate = false;
+        }
+	}
+
+    void BuildTriangle()
+    {
+        MeshFilter mFilter = GetComponent<MeshFilter>();
+        Mesh mesh = mFilter.mesh;
+
+        mesh.vertices = new Vector3[3] { new Vector3(.5f, -.5f), new Vector3(-.5f, -.5f), new Vector3(-.5f, .5f) };
+        mesh.triangles = new int[3] { 0, 1, 2 };
+        mesh.uv = new Vector2[3] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1) };
+    }
+}
